4ddaa1a826cdb70c26cf6a1e5d5aa9273e62f226
Josh <sykopomp@sykosomatic.org>
2008-08-27 18:59:15 -0400

    Redid documentation style for sykosomatic. Much nicer now, I think.

--- packages.lisp
+++ packages.lisp
@@ -14,6 +14,7 @@
 
 ;; You should have received a copy of the GNU General Public License
 ;; along with sykosomatic.  If not, see <http://www.gnu.org/licenses/>.
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defpackage #:sykosomatic
   (:use :cl :cl-store)
