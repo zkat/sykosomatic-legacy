1d01b2a7b97f3b2d0fc523ecb99a88feb7192fb7
Kat Marchán <kzm@sykosomatic.org>
2008-09-14 20:03:03 -0400

    Relicensed the project to AGPL. FREEDOOOMMM *froths at mouth*

--- src/tests/parser.lisp
+++ src/tests/parser.lisp
@@ -1,4 +1,19 @@
-;; -*- common-lisp -*-
+;; Copyright 2008 Rudolf Olah
+
+;; This file is part of sykosomatic
+
+;; sykosomatic is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; sykosomatic is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with sykosomatic.  If not, see <http://www.gnu.org/licenses/>.
 
 (in-package #:sykosomatic)
 
