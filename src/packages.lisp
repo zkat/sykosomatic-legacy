ba9c300d19084595f43f3c3424b14b3b396b7a5e
Josh <sykopomp@sykosomatic.org>
2008-10-04 14:08:33 -0400

    Beginning port to bknr.datastore.
    Added bknr.datastore to packages.lisp and sykosomatic.asd. Everything loads
    properly. Had to take out cl-store from the main package because of naming
    conflicts.

--- src/packages.lisp
+++ src/packages.lisp
@@ -17,6 +17,6 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defpackage #:sykosomatic
-  (:use :cl :cl-store :cl-cont :xmls :fiveam)
+  (:use :cl :cl-cont :xmls :fiveam :bknr.datastore)
   (:import-from :bordeaux-threads :make-thread :destroy-thread :all-threads :with-lock-held)
   (:import-from :ironclad :byte-array-to-hex-string :digest-sequence :ascii-string-to-byte-array))
