99893202d088d3fbe0e7f6e2a4c9d093deacc273
Josh <sykopomp@sykosomatic.org>
2008-10-08 02:03:24 -0400

    finished adding everything to src/packages.lisp
    Now to start yanking shit out of there.

--- src/packages.lisp
+++ src/packages.lisp
@@ -9,6 +9,10 @@
   (:import-from :ironclad :byte-array-to-hex-string :digest-sequence :ascii-string-to-byte-array)
   (:export
 
+   ;; general
+   :defun/cc
+   :define-persistent-class
+
    ;; config.lisp
    :*game-directory*
    :*db-directory*
@@ -22,10 +26,35 @@
 
    ;; login module
    ;; ------------
+   ;;
+   ;; account.lisp
+   :<account>
+   :username
+   :account-with-name
+   :all-accounts
+   :password
+   :email
+   :account-with-email
+   :avatars
+   :clients
+   :account-type
+   :accounts-with-type
+   :known-ips
+   :account-creation-error
+   :account-exists-error
+
+   ;; login-avatar.lisp
+   :choose-avatar
+   :avatar-main-loop
+   :handle-avatar-input
+
+   ;; login.lisp
+   :login-menu
+   :account-menu
 
    ;; objects module
    ;; --------------
-   ;;.
+   ;;
    ;; game-object.lisp
    :<game-object>
    :name
