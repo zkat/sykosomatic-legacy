bc9e773b36695dd24155e439c8716bfb545113e6
Josh Marchán <sykopomp@sykosomatic.org>
2010-07-26 22:51:14 -0500

    Skeleton for some basic game objects.

---
+++ src/game-objects/human.lisp
@@ -0,0 +1,25 @@
+;; Copyright 2008-2010 Kat Marchán
+
+;; This file is part of sykosomatic
+
+;; sykosomatic is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; sykosomatic is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with sykosomatic.  If not, see <http://www.gnu.org/licenses/>.
+
+;; human.lisp
+;;
+;; humans
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-package :sykosomatic)
+
+(defclass human (mobile)
+  ())
