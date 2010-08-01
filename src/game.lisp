a079f0c5f547d3f4f9de0948f675d71bbe737aad
Josh Marchán <sykopomp@sykosomatic.org>
2010-08-01 02:33:28 -0500

    Added game.lisp

---
+++ src/game.lisp
@@ -0,0 +1,45 @@
+;; Copyright 2008-2010 Kat Marchán
+
+;; This file is part of sykosomatic
+
+;; sykosomatic is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; sykosomatic is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with sykosomatic.  If not, see <http://www.gnu.org/licenses/>.
+
+;; game.lisp
+;;
+;; Implements a basic game
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-package :sykosomatic)
+
+(defclass game (engine)
+  ()
+  (:default-initargs
+   :providers (list (make-instance 'tcp-service-provider))))
+
+(defmethod init ((game game))
+  (setf (newbie-room game) (make-instance 'room)
+        (rooms game) (list (newbie-room game))))
+
+(defmethod handle-player-command ((soul soul) input &aux (input (string-cleanup input)))
+  (unless (zerop (length input))
+    (cond ((string-equal input "look")
+           (format (client soul)
+                   "You see a double rainbow all the way across the sky. So intense.~%"))
+          ((string-equal input "quit")
+           (disconnect (client soul) :close))
+          (t
+           (format (client soul) "~&You say, \"~A\"~%" input)
+           (broadcast-to-room (client soul)  "~&~A says, \"~A\"~%" (username (account soul)) input)))))
+
+(defun main ()
+  (run (make-instance 'game)))
