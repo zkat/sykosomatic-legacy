571963244f0aedd9b3090de41d6af9c4a63dc042
Kat Marchán <kzm@sykosomatic.org>
2010-08-07 18:20:51 -0500

    Vocabulary code overhaul
    There is now a single *vocabulary* hash table, which is checked for various strings. Different words
    may be associated with different categories.

--- src/vocabulary.lisp
+++ src/vocabulary.lisp
@@ -24,156 +24,57 @@
 
 (declaim (optimize debug))
 
-(defvar *vocab-directory* (merge-pathnames #P".sykosomatic/vocab/" (user-homedir-pathname))
-  "Vocabulary directory")
-
-;;;
-;;; Vocab vars
-;;;
-
-(defvar *verbs* (make-hash-table :test #'equalp)
-  "This is a dotted list right now. The CAR is a string, CDR the function.")
-
-(defvar *adverbs* (make-hash-table :test #'equalp)
-  "This contains a HASH TABLE of all available ADVERBS.")
-
-(defvar *articles* (make-hash-table :test #'equalp)
-  "A table of articles, as strings.")
-
-(defvar *prepositions* (make-hash-table :test #'equalp)
-  "A table of strings which represent prepositions.")
-
-(defvar *pronouns* (make-hash-table :test #'equalp)
-  "Table of pronouns, as strings.")
-
-(defvar *conjunctions* (make-hash-table :test #'equalp)
-  "Table of possible conjunctions. Go wild.")
-
-(defvar *cardinal-numbers* (make-hash-table :test #'equalp)
-  "Table of words representing cardinal numbers, and the number they map to.")
-
-(defvar *ordinal-numbers* (make-hash-table :test #'equalp)
-  "Table of words representing ordinal numbers, and the number they map to.")
-
-(defvar *plural-exceptions* (make-hash-table :test #'equalp)
-  "Table of exceptions to the English WORD+(e)s pluralization rule.")
+(defvar *vocabulary* (make-hash-table :test #'equalp)
+  "A hash table holding the vocabulary for the game. Keys are strings mapping to a list of word types")
 
 ;;;
 ;;; Load/Save
 ;;;
 
-;; TODO: Clean these up. They suck.
 (defun save-vocabulary ()
   "Saves all the nice vocabulary words :)"
-  (cl-store:store *verbs* (ensure-directories-exist (merge-pathnames #P"verbs.db" *vocab-directory*)))
-  (cl-store:store *adverbs* (ensure-directories-exist (merge-pathnames #P"adverbs.db" *vocab-directory*)))
-  (cl-store:store *articles* (ensure-directories-exist (merge-pathnames #P"articles.db" *vocab-directory*)))
-  (cl-store:store *prepositions* (ensure-directories-exist (merge-pathnames #P"prepositions.db" *vocab-directory*)))
-  (cl-store:store *pronouns* (ensure-directories-exist (merge-pathnames #P"pronouns.db" *vocab-directory*)))
-  (cl-store:store *conjunctions* (ensure-directories-exist (merge-pathnames #P"conjunctions.db" *vocab-directory*)))
-  (cl-store:store *cardinal-numbers* (ensure-directories-exist (merge-pathnames #P"cardinal-numbers.db" *vocab-directory*)))
-  (cl-store:store *ordinal-numbers* (ensure-directories-exist (merge-pathnames #P"ordinal-numbers.db" *vocab-directory*)))  
-  (cl-store:store *plural-exceptions* (ensure-directories-exist (merge-pathnames #P"plural-exceptions.db" *vocab-directory*)))
   (format t "Vocabulary saved."))
 
 (defun load-vocabulary ()
   "Loads saved vocab files into their respective variables."
-  (setf *verbs* (cl-store:restore (merge-pathnames #P"verbs.db" *vocab-directory*)))
-  (setf *adverbs* (cl-store:restore (merge-pathnames #P"adverbs.db" *vocab-directory*)))
-  (setf *articles* (cl-store:restore (merge-pathnames #P"articles.db" *vocab-directory*)))
-  (setf *prepositions* (cl-store:restore (merge-pathnames #P"prepositions.db" *vocab-directory*)))
-  (setf *pronouns* (cl-store:restore (merge-pathnames #P"pronouns.db" *vocab-directory*)))
-  (setf *conjunctions* (cl-store:restore (merge-pathnames #P"conjunctions.db" *vocab-directory*)))
-  (setf *cardinal-numbers* (cl-store:restore (merge-pathnames #P"cardinal-numbers.db" *vocab-directory*)))
-  (setf *ordinal-numbers* (cl-store:restore (merge-pathnames #P"ordinal-numbers.db" *vocab-directory*)))
-  (setf *plural-exceptions* (cl-store:restore (merge-pathnames #P"plural-exceptions.db" *vocab-directory*)))
   (format t "Vocabulary loaded."))
 
 ;;;
 ;;; Database Management
 ;;;
+(defun remove-word (word)
+  (remhash word *vocabulary*)
+  word)
 
-(defun add-verb (string function)
+(defun add-category-to-word (string category)
+  (pushnew category (gethash string *vocabulary*)))
+
+(defun remove-category-from-word (string category)
+  (deletef (gethash string *vocabulary*) category))
+
+(defun category-in-word-p (string category)
+  (when (find category (gethash string *vocabulary*))
+    string))
+
+(defun add-verb (word)
   "Associates STRING with FUNCTION and adds the new verb to *VERBS*"
-  (setf (gethash string *verbs*) function))
+  (add-category-to-word word :verb))
 
-(defun remove-verb (string)
+(defun remove-verb (word)
   "Removes the VERB that corresponds to STRING from *VERBS*"
-  (remhash string *verbs*))
+  (remove-category-from-word word :verb))
 
-(defun refresh-verb (string function)
-  "Associates STRING with FUNCTION and adds it to *VERBS*,
-removing all previous associations with STRING"
-  (remove-verb string)
-  (add-verb string function))
-
-
-;;;
-;;; Predicates
-;;;
-
-(defun articlep (word)
-  "Is WORD an ARTICLE?"
-  (gethash word *articles*))
+(defun (setf verbp) (verbp word)
+  (if verbp
+      (add-verb word)
+      (remove-verb word))
+  verbp)
 
 (defun verbp (word)
   "Is WORD a VERB?"
-  (gethash word *verbs*))
+  (category-in-word-p word :verb))
 
 (defun chat-string-p (word)
   "Is WORD a CHAT-WORD?"
+  ;; Ew..
   (char-equal #\' (char word 0)))
-
-(defun prepositionp (word)
-  "Is WORD a PREPOSITION?"
-  (gethash word *prepositions*))
-
-(defun adverbp (word)
-  "Is WORD an ADVERB?"
-  (gethash word *adverbs*))
-
-(defun conjunctionp (word)
-  "Is WORD a CONJUNCTION?"
-  (gethash word *conjunctions*))
-
-(defun possessivep (word)
-  "Is WORD in possessive form?
-This function checks for s' or 's form of possessives in English."
-  (or (string-equal word "my")
-      (string-equal word "his")
-      (string-equal word "her")
-      (string-equal word "its")
-      (string-equal word "their")
-      (when (> (length word) 2)
-        (let ((second-to-last-letter (elt word(- (length word) 2)))
-              (last-letter (elt word (- (length word) 1))))
-          (or (and (equal second-to-last-letter #\')
-                   (equal last-letter #\s))
-              (and (equal second-to-last-letter #\s)
-                   (equal last-letter #\')))))))
-
-(defun pronounp (word)
-  "Is WORD a PRONOUN?"
-  (gethash word *pronouns*))
-
-;; TODO: This should also deal with numbers in their "#th" form
-(defun ordinal-number-p (word)
-  "Is WORD an ORDINAL NUMBER?
-This function checks for the full-word version,
-as well as number+th/st/nd/rd form."
-  (or (gethash word *ordinal-numbers*)
-      (multiple-value-bind (integer stop) (parse-integer word :junk-allowed t) 	
-        (and (numberp integer)
-             (or 
-              (string-equal (subseq word stop) "st")
-              (string-equal (subseq word stop) "nd")
-              (string-equal (subseq word stop) "rd")
-              (string-equal (subseq word stop) "th"))))))
-
-(defun cardinal-number-p (word)
-  "Is WORD a CARDINAL NUMBER?
-This function checks for the full-word version, as well as the plain number version."
-  (or (gethash word *cardinal-numbers*)
-      (multiple-value-bind (integer stop) (parse-integer word :junk-allowed t)
-        (and (numberp integer)
-             (= (length (subseq word stop)) 0)))))
