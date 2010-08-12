0114d0406430595dbac4e7ae3060d50b65f96bdc
Kat Marchán <kzm@sykosomatic.org>
2010-08-11 21:51:23 -0500

    Moved verb definition stuff out to its own file.

---
+++ src/verbs.lisp
@@ -0,0 +1,78 @@
+;; Copyright 2008-2010 Kat Marchán
+
+;; This file is part of sykosomatic
+
+;; sykosomatic is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU Affero General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; sykosomatic is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU Affero General Public License for more details.
+
+;; You should have received a copy of the GNU Affero General Public License
+;; along with sykosomatic.  If not, see <http://www.gnu.org/licenses/>.
+
+;; verbs.lisp
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-package :sykosomatic)
+
+(defclass verb (document) ())
+(def-doc-accessors verb
+  (function-definition "function-definition")
+  (word "word"))
+
+(defmethod (setf function-definition) :around (new-value (verb verb))
+  (if (stringp new-value)
+      (call-next-method)
+      (call-next-method (prin1-to-string new-value) verb)))
+
+(defun make-verb (word function-definition)
+  (let ((uuid (gen-uuid)))
+    (put-document *db* uuid
+                  (mkhash "type" "verb"
+                          "word" word
+                          "function-definition" (prin1-to-string function-definition)))
+    (make-instance 'verb :document (get-document *db* uuid))))
+
+(defun ensure-verb-design-doc ()
+  (or (handler-case
+          (get-document *db* "_design/verbs")
+        (document-not-found () nil))
+      (put-document *db* "_design/verbs"
+                    (mkhash "language" "common-lisp"
+                            "views" (mkhash "by_word"
+                                            (mkhash "map"
+                                                    (prin1-to-string
+                                                     '(lambda (doc &aux (type (hashget doc "type")))
+                                                       (when (equal type "verb")
+                                                         (emit (hashget doc "word")
+                                                               (hashget doc "function-definition")))))))))))
+
+(defun find-verb (word)
+  (let* ((response (get-document *db* "_design/verbs/_view/by_word" :key word))
+         (rows (hashget response "rows")))
+    (when rows
+      (make-instance 'verb
+                     :document (get-document *db* (hashget (car rows) "id"))))))
+
+(defun add-verb (word function-definition)
+  "Associates STRING with FUNCTION and adds the new verb to *VERBS*"
+  (add-category-to-word word "verb")
+  (let ((existing-doc (find-verb word)))
+    (if existing-doc
+        (progn
+          (setf (function-definition existing-doc) (prin1-to-string function-definition))
+          (save existing-doc))
+        (make-verb word function-definition))))
+
+(defun remove-verb (word)
+  "Removes the VERB that corresponds to STRING from *VERBS*"
+  (remove-category-from-word word "verb"))
+
+(defmacro defverb (name (actor direct-object indirect-object adverb) &body body)
+  `(add-verb (string-downcase (string ',name))
+             '(lambda (,actor ,direct-object ,indirect-object ,adverb) ,@body)))
--- src/vocabulary.lisp
+++ src/vocabulary.lisp
@@ -86,59 +86,6 @@
   (when (find category (hashget *vocabulary* string) :test #'string-equal)
     string))
 
-(defclass verb (document) ())
-(def-doc-accessors verb
-  (function-definition "function_definition")
-  (word "word"))
-
-(defmethod (setf function-definition) :around (new-value (verb verb))
-  (if (stringp new-value)
-      (call-next-method)
-      (call-next-method (prin1-to-string new-value) verb)))
-
-(defun make-verb (word function-definition)
-  (let ((uuid (gen-uuid)))
-    (put-document *db* uuid
-                  (mkhash "type" "verb"
-                          "word" word
-                          "function_definition" (prin1-to-string function-definition)))
-    (make-instance 'verb :document (get-document *db* uuid))))
-
-(defun ensure-verb-design-doc ()
-  (or (handler-case
-          (get-document *db* "_design/verbs")
-        (document-not-found () nil))
-      (put-document *db* "_design/verbs"
-                    (mkhash "language" "common-lisp"
-                            "views" (mkhash "by_word"
-                                            (mkhash "map"
-                                                    (prin1-to-string
-                                                     '(lambda (doc &aux (type (hashget doc "type")))
-                                                       (when (equal type "verb")
-                                                         (emit (hashget doc "word")
-                                                               (hashget doc "function_definition")))))))))))
-
-(defun find-verb (word)
-  (let* ((response (get-document *db* "_design/verbs/_view/by_word" :key word))
-         (rows (hashget response "rows")))
-    (when rows
-      (make-instance 'verb
-                     :document (get-document *db* (hashget (car rows) "id"))))))
-
-(defun add-verb (word function-definition)
-  "Associates STRING with FUNCTION and adds the new verb to *VERBS*"
-  (add-category-to-word word "verb")
-  (let ((existing-doc (find-verb word)))
-    (if existing-doc
-        (progn
-          (setf (function-definition existing-doc) (prin1-to-string function-definition))
-          (save existing-doc))
-        (make-verb word function-definition))))
-
-(defun remove-verb (word)
-  "Removes the VERB that corresponds to STRING from *VERBS*"
-  (remove-category-from-word word "verb"))
-
 (defun verbp (word)
   "Is WORD a VERB?"
   (category-in-word-p word "verb"))
