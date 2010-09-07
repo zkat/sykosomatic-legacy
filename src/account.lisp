8da6d1dd9cdc7e3886b525446d242fe4f6bdec17
Kat Marchán <kzm@sykosomatic.org>
2010-09-06 21:36:38 -0500

    Added account protocol.

--- src/account.lisp
+++ src/account.lisp
@@ -23,3 +23,99 @@
   (:use :cl))
 (cl:in-package :sykosomatic.account)
 
+;;;
+;;; Account protocol
+;;;
+
+;; Accessors
+(defgeneric account-username (account))
+(defgeneric account-password (account))
+(defgeneric account-email (account))
+(defgeneric account-engine (account))
+
+(defgeneric find-account (engine username)
+  (:documentation "Returns an account object associated with USERNAME. If there is no such account,
+NIL is returned."))
+
+(defgeneric ensure-account (engine username password email)
+  (:documentation "Creates a new account."))
+
+(defgeneric hash-password (engine password))
+
+(defgeneric validate-username (engine username)
+  (:method (engine username)
+    (declare (ignore engine username))
+    t))
+(defgeneric validate-password (engine password)
+  (:method (engine password)
+    (declare (ignore engine password))
+    t))
+(defgeneric validate-email (engine email)
+  (:method (engine email)
+    (declare (ignore engine email))
+    t))
+
+(defgeneric verify-password-using-engine (engine account password)
+  (:documentation "Returns a true value if PASSWORD is the valid password for ACCOUNT.")
+  (:method (engine account password)
+    (when (string= (account-password account)
+                   (hash-password engine password))
+      account)))
+
+(defun verify-password (account password)
+  "Trampoline function for VERIFY-ACCOUNT-USING-ENGINE. Confirms that PASSWORD is the correct
+password for ACCOUNT."
+  (verify-account-using-engine (account-engine account) account password))
+
+(defgeneric account-exists-p (engine username)
+  (:documentation "Returns a true value if an account associated with USERNAME exists.")
+  (:method (engine username)
+    (find-account engine username)))
+
+;;;
+;;; Documents as accounts
+;;;
+;; (defun find-account-document (db username)
+;;   (let* ((response (get-document db "_design/accounts/_view/by_username" :key username))
+;;          (rows (at response "rows")))
+;;     (when rows (at (car rows) "value"))))
+
+;; (defmethod find-account (engine username)
+;;   (make-instance 'document :engine engine
+;;                  :document 
+;;                  (find-account-document (engine-db engine) username)))
+
+;; (defmethod ensure-account (engine username password email)
+;;   (when (account-exists-p engine username)
+;;     (error "That username is already being used."))
+;;   (make-instance 'document
+;;                  :engine engine
+;;                  :document
+;;                  (put-document (engine-db engine) (gen-uuid)
+;;                                (mkhash "type" "account"
+;;                                        "username" username
+;;                                        "password" password
+;;                                        "email" email))))
+
+;; (defmethod account-username ((doc document))
+;;   (document-slot-value doc "username"))
+;; (defmethod account-password ((doc document))
+;;   (document-slot-value doc "password"))
+;; (defmethod account-email ((doc document))
+;;   (document-slot-value doc "email"))
+;; (defmethod account-engine ((doc document))
+;;   (document-engine doc))
+
+;; (defun ensure-account-design-doc (engine &aux (db (engine-db engine)))
+;;   (or (handler-case
+;;           (get-document db "_design/accounts")
+;;         (document-not-found () nil))
+;;       (put-document db "_design/accounts"
+;;                     (mkhash "language" "common-lisp"
+;;                             "views" (mkhash "by_username"
+;;                                             (mkhash "map"
+;;                                                     (prin1-to-string
+;;                                                      '(lambda (doc &aux (type (at doc "type")))
+;;                                                        (when (equal type "account")
+;;                                                          (emit (at doc "username")
+;;                                                                doc))))))))))
